37d8a775f2b75791b2668c2fda858125f320efaa
shevtsiv <rostykshevtsiv@gmail.com>
2019-02-18 20:39:22 +0200

    Support unlimited amount of Git accounts

--- src/config.rs
+++ src/config.rs
@@ -5,36 +5,17 @@ use std::io::Read;
 
 #[derive(Deserialize)]
 pub struct Config {
-    first: Option<AccountSection>,
-    second: Option<AccountSection>,
-    third: Option<AccountSection>,
+    account: Vec<AccountSection>,
 }
 
 impl Config {
-    pub fn first_account(&self) -> &AccountSection {
-        self.first.as_ref().expect("There is no first account section specified in the config!")
-    }
-
-    pub fn second_account(&self) -> &AccountSection {
-        self.second.as_ref().expect("There is no second account section specified in the config!")
-    }
-
-    // The third account is not always required but possible, so we do not unwrap it here
-    pub fn third_account(&self) -> Option<&AccountSection> {
-        self.third.as_ref()
-    }
-
     pub fn get_account_by_name(&self, name: &str) -> Option<&AccountSection> {
-        if self.first_account().get_name() == name {
-            Some(self.first_account())
-        } else if self.second_account().get_name() == name {
-            Some(self.second_account())
-        } else {
-            if self.third_account().is_some() && self.third_account().unwrap().get_name() == name {
-                return self.third_account();
+        for account in &self.account {
+            if account.get_name() == name {
+                return Some(account);
             }
-            None
         }
+        None
     }
 }
 
